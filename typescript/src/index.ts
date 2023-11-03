8482d7b3c815962bc95b5178893f3cbe79bb6e0b
Mitch Oz <divine_comedian@protonmail.com>
2023-11-03 11:11:09 -0600

    make nice minting example

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,11 +1,19 @@
 import { Relayer } from 'defender-relay-client';
-import { RelayerModel, RelayerParams } from 'defender-relay-client/lib/relayer';
+import { Address, RelayerModel, RelayerParams } from 'defender-relay-client/lib/relayer';
 
+const niceMinter: Address = '0x90552d25e322e0f3fa9ad44024fe4d66f9918cad';
 // Entrypoint for the Autotask
 export async function handler(credentials: RelayerParams) {
+  
   const relayer = new Relayer(credentials);
   const info: RelayerModel = await relayer.getRelayer();
   console.log(`Relayer address is ${info.address}`);
+  const txRes = await relayer.sendTransaction({
+    to: niceMinter,
+    data: '0x40c10f19000000000000000000000000826976d7c600d45fb8287ca1d7c76fc8eb7320300000000000000000000000000000000000000000000000000de0b6b3a7640000',
+    gasLimit: '1000000',
+  })
+  console.log(`Transaction sent: ${txRes.hash}`);
 }
 
 // Sample typescript type definitions
